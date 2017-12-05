8f48f614bb4dcb3c5cb604c8323b9674e9bd3dbb
Joao Pedro <pribeiro.rib@gmail.com>
2017-12-05 20:18:29 -0200

    folder structure organized

--- etcht-a-screcht/script.js
+++ etcht-a-screcht/script.js
@@ -2,7 +2,11 @@ $(document).ready(function(){
   for (var i = 1; i < 10000; i++){
       $("section").append('<div></div>');
   };
-  $('div').hover(function(){
+  $('div').mousedown(function(){
     $(this).css('background', 'red')
+    timeout = setInterval(function(){
+      $(this).css('background', 'red')
+    }, 500);
+    return false
   });
 });
