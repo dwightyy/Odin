4453230b244aad0898d3e9db188b3160a2b3643f
Joao Pedro <pribeiro.rib@gmail.com>
2017-12-05 22:16:50 -0200

    atualizado

--- etcht-a-screcht/script.js
+++ etcht-a-screcht/script.js
@@ -1,12 +1,21 @@
+var counter = 0
 $(document).ready(function(){
-  for (var i = 1; i < 10000; i++){
+  for (var i = 1; i < 257; i++){
       $("section").append('<div></div>');
   };
-  $('div').mousedown(function(){
-    $(this).css('background', 'red')
-    timeout = setInterval(function(){
-      $(this).css('background', 'red')
-    }, 500);
-    return false
+  if(counter == 0){
+    $('div').click(function(){
+      $('div').hover(function(){
+        $(this).css('background', 'red')
+      });
+    });
+}
+  x = $( window ).height();
+
+  $('p').css('top',100)
+
+  $('.btn').click(function(){
+    $("div").css('background', 'blue')
+      counter = 1
   });
 });
